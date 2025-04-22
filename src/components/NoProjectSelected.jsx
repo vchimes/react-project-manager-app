2c2247618cd3cd086e4abdc398d0d21af5b0d13f
Vince Chimento <vchimento@gmail.com>
2025-04-22 16:14:31 -0400

    create Button, NoProjectSelected components

---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,21 @@
+import image from '../assets/no-projects.png';
+import Button from './Button.jsx';
+
+export default function NoProjectSelected() {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img
+        className="w-16 h-16 object-contain mx-auto"
+        src={image}
+        alt="An empty task list"
+      />
+      <h2 className="font-bold text-xl text-stone-500 my-4">
+        No Project Selected
+      </h2>
+      <p className="text-stone-400 mb-8">
+        Select a project or get started with a new one
+      </p>
+      <Button>Create new project</Button>
+    </div>
+  );
+}
