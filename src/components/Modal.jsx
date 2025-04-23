8faa435e1004e84cfb352a99692fead35479a1dc
Vince Chimento <vchimento@gmail.com>
2025-04-22 22:51:01 -0400

    create Modal component to handle invalid input

---
+++ src/components/Modal.jsx
@@ -0,0 +1,24 @@
+import { useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
+
+export default function Modal({ ref, children, buttonCaption }) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return createPortal(
+    <dialog ref={dialog}>
+      {children}
+      <form method="dialog">
+        <button>{buttonCaption}</button>
+      </form>
+    </dialog>,
+    document.getElementById('modal-root')
+  );
+}
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,8 +1,11 @@
 import { useRef } from 'react';
 
 import Input from './Input.jsx';
+import Modal from './Modal.jsx';
 
 export default function NewProject({ onAdd }) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -12,6 +15,15 @@ export default function NewProject({ onAdd }) {
     const enteredDescription = title.current.value;
     const enteredDueDate = dueDate.current.value;
 
+    if (
+      enteredTitle.trim() === '' ||
+      enteredDescription.trim() === '' ||
+      enteredDueDate.trim() === ''
+    ) {
+      modal.current.open();
+      return;
+    }
+
     onAdd({
       title: enteredTitle,
       description: enteredDescription,
@@ -20,25 +32,32 @@ export default function NewProject({ onAdd }) {
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={handleSave}
-            className="bg-stone-800 text-stone-50 px-6 py-2 rounded-md text-lg hover:bg-stone-950"
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <Input type="text" ref={title} label="Title" />
-      <Input ref={description} label="Description" textarea />
-      <Input type="date" ref={dueDate} label="Due Date" />
-    </div>
+    <>
+      <Modal ref={modal} buttonCaption="Close">
+        <h2>🚨Invalid inputs🚨</h2>
+        <p>A value must be entered for every field.</p>
+      </Modal>
+      ;
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleSave}
+              className="bg-stone-800 text-stone-50 px-6 py-2 rounded-md text-lg hover:bg-stone-950"
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <Input type="text" ref={title} label="Title" />
+        <Input ref={description} label="Description" textarea />
+        <Input type="date" ref={dueDate} label="Due Date" />
+      </div>
+    </>
   );
 }
