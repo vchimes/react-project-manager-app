e4448d4230c78cd213a26ef5aaaa4394235423fe
Vince Chimento <vchimento@gmail.com>
2025-05-07 13:43:27 -0400

    create SelectedProject component for viewing projects

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,22 @@ import { useState } from 'react';
 import Sidebar from './components/Sidebar.jsx';
 import NewProject from './components/NewProject.jsx';
 import NoProjectSelected from './components/NoProjectSelected.jsx';
+import SelectedProject from './components/SelectedProject.jsx';
 
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: [],
   });
+
+  function handleSelectProject(id) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  }
 
   function handleStartAddProject() {
     setProjectsState((prevState) => {
@@ -46,7 +56,11 @@ function App() {
 
   console.log(projectsState);
 
-  let content;
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectId
+  );
+
+  let content = <SelectedProject project={selectedProject} />;
 
   if (projectsState.selectedProjectId === null) {
     content = (
@@ -61,6 +75,7 @@ function App() {
       <Sidebar
         projects={projectsState.projects}
         onAdd={handleStartAddProject}
+        onSelectProject={handleSelectProject}
       />
       {content}
     </main>
